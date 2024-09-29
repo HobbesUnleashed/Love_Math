e2b62640ad14e87fff13bb6313d24cc7d95331bb
Seamus Leacy <sleacy@live.co.uk>
2024-09-29 20:53:18 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+//Wait for the DOM to finish loading before running this game
+//Get the button elements and add event listeners to them
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if (this.getAttribute('data-type') === 'submit') {   //If button that was just clicked IS submit -- called by using the custom data attribute created
+                alert('You clicked submit');                     //Show this alert!!
+            } else {                                             //Otherwise do the following
+                let gameType = this.getAttribute('data-type');   //Take the custom attribute and assign it to a variable
+                alert(`You clicked ${gameType}`);                //Pass an alert to say which game was picked -- using template literals and the variable
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }

a672a5f694f141f3334ca85608e538b8dfd22af4
Seamus Leacy <sleacy@live.co.uk>
2024-10-04 08:38:19 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,43 @@
-//Wait for the DOM to finish loading before running this game
-//Get the button elements and add event listeners to them
-document.addEventListener('DOMContentLoaded', function() {
-    let buttons = document.getElementsByTagName('button');
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener('click', function() {
-            if (this.getAttribute('data-type') === 'submit') {   //If button that was just clicked IS submit -- called by using the custom data attribute created
-                alert('You clicked submit');                     //Show this alert!!
-            } else {                                             //Otherwise do the following
-                let gameType = this.getAttribute('data-type');   //Take the custom attribute and assign it to a variable
-                alert(`You clicked ${gameType}`);                //Pass an alert to say which game was picked -- using template literals and the variable
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {      //If button that was just clicked IS submit -- called by using the custom data attribute created
+                alert("You clicked Submit!");                       //Show this alert!!
+            } else {                                                //Otherwise do the following
+                let gameType = this.getAttribute("data-type");      //Take the custom attribute and assign it to a variable
+                //alert(`You clicked ${gameType}`);                   Pass an alert to say which game was picked -- using template literals and the variable
+                runGame(gameType);                                  //Start the game with whatever gameType was selected - removes need for previous line
             }
-        })
+        });
     }
-})
+    //Addition game to start as soon as the game is loaded
+    runGame("addition");
+
+});
 
 /**
- * The main game 'loop' called when the script is first loaded
- * and after the users answer has been processed
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
  */
-function runGame() {
-    //Creates two random numbers between 1-25
-    let num1 = Math.floor(Math.random()*25);
-    let num2 = Math.floor(Math.random()*25);
+function runGame(gameType) {                                        //gameType passed as an argument for the function
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;                  // Math.floor = round down
+    let num2 = Math.floor(Math.random() * 25) + 1;                  // Math.random = 0-1 as decimals
+    // * 25 to make it 0-25. +1 to make it 1-25
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -41,18 +56,18 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
-function displaySubtractQuestion) {
+function displaySubtractQuestion() {
 
 }
 
 function displayMultiplyQuestion() {
-
-}
-
-function displayDivisionQuestion() {
-
+    
 }

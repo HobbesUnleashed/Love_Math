71e4f4cc674475689ff0d6f72d90c71e9fad53f3
Seamus Leacy <sleacy@live.co.uk>
2024-10-04 09:31:52 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,8 +52,10 @@ function checkAnswer() {
 
     if (isCorrect) {                                                            //If correct - display this message to user
         alert('Hey! You got it right! :D');
+        incrementScore();
     } else {                                                                    //Otherwise show their answer and the correct
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);                                               //Run the next game using the second part of the calculatedAnswer array
@@ -76,12 +78,20 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
